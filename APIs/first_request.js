8f3cf2e784d12a19f0d2653c5e80361741aa9706
Jayne Jacobs <jaynejacobs@comcast.net>
2017-11-02 13:23:21 +0000

    This is my API Changes

--- APIs/first_request.js
+++ APIs/first_request.js
@@ -2,7 +2,21 @@
 var request = require("request");
 request('http://jaynejacobs.com' , function(error, response, body) {
     if(error){
-        console.log("Somethint is wrong");
+        console.log("Something is wrong");
+        console.log(error);
+    }else{
+        if(response.statusCode == 200){
+            //THINGS WORKED!!
+            console.log(body);
+        }
+    }
+            
+});
+
+
+request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys' , function(error, response, body) {
+    if(error){
+        console.log("Asked for Google but no go");
         console.log(error);
     }else{
         if(response.statusCode == 200){
