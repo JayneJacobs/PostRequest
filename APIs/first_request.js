e9635d285351f687b2aafae3b66c1788be23755c
Jayne Jacobs <jaynejacobs@comcast.net>
2017-11-16 13:10:08 +0000

    Added Songs and Express Apps and APIs

--- APIs/first_request.js
+++ APIs/first_request.js
@@ -14,15 +14,10 @@ request('http://jaynejacobs.com' , function(error, response, body) {
 });
 
 
-request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys' , function(error, response, body) {
-    if(error){
-        console.log("Asked for Google but no go");
-        console.log(error);
-    }else{
-        if(response.statusCode == 200){
-            //THINGS WORKED!!
-            console.log(body);
-        }
-    }
+request('https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22maui%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys' , function(error, response, body) {
+    if(!error && response.statusCode == 200){
+        var parsedData = JSON.parse(body);
+        console.log(" Sunset in Hawaii is at:", parsedData["query"]["results"]["channel"]["astronomy"]["sunset"]);
+    }  
             
 });
